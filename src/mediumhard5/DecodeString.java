6995be73d44fedf1c9b660255e70cf67f7a0f97d
Sandeep N <sandeep.kumar.nagamalli@ibm.com>
2026-07-27 19:31:27 +0530

    change DecodeString.java

--- src/mediumhard5/DecodeString.java
+++ src/mediumhard5/DecodeString.java
@@ -1,5 +1,79 @@
 // https://leetcode.com/problems/decode-string/description/?envType=problem-list-v2&envId=rabvlt31
 
+//DecodeString.java
+//https://github.com/nikoo28/java-solutions/blob/master/src/main/java/leetcode/medium/DecodeString.java
+//https://www.youtube.com/watch?v=E9qHRcQXmDk
+//eg:2[a3[c2[x]]y] 
+
+//1st approach is solving inner most, then traversing back and solving next inner etc....it will take long time.
+//So we use stack to solve it in linear time
+
+
+
+package leetcode.medium;
+
+import java.util.Stack;
+
+public class DecodeString {
+
+  public String decodeString(String s) {
+
+    Stack<Integer> numStack = new Stack<>();
+    Stack<String> stringStack = new Stack<>();
+    int k = 0;
+
+    for (char c : s.toCharArray()) {
+
+      if (Character.isDigit(c)) {
+        k = (k * 10) + (c - '0');
+        continue;
+      }
+
+      if (c == '[') {
+        numStack.push(k);
+        k = 0;
+        stringStack.push(String.valueOf(c));
+        continue;
+      }
+
+      if (c != ']') {
+        stringStack.push(String.valueOf(c));
+        continue;
+      }
+
+      StringBuilder temp = new StringBuilder();
+      while (!stringStack.peek().equals("["))
+        temp.insert(0, stringStack.pop());
+
+      // remove the "["
+      stringStack.pop();
+
+      // Get the new string
+      StringBuilder replacement = new StringBuilder();
+      int count = numStack.pop();
+      for (int i = 0; i < count; i++)
+        replacement.append(temp);
+
+      // Add it to the stack
+      stringStack.push(replacement.toString());
+    }
+
+    StringBuilder result = new StringBuilder();
+    while (!stringStack.empty()) {
+      result.insert(0, stringStack.pop());
+    }
+    return result.toString();
+  }
+
+}
+
+
+
+
+
+
+
+//*********************************************
 package mediumhard5;
 
 import java.util.*;
